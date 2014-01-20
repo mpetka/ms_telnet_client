d84e6722373d9bd046963700861cd9ddfc2b70ef
Sölvi Páll Ásgeirsson <solvi@rb.is>
2014-01-20 14:05:34 +0000

    Metadata added

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+name             'ms_telnet_client'
+maintainer       'Sölvi Páll Ásgeirsson'
+maintainer_email 'solvip@gmail.com'
+license          'All rights reserved'
+description      'Ensures that the Telnet-Client feature is present on Windows nodes'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports 'windows'
+
+depends 'windows'
